b2131d8b4e1596e229cab1acfacfdd5d3ee1ff58
Dariia <im.dari.it@gmail.com>
2026-01-19 10:54:53 +0100

    Fix safesearch parameter in Pixabay API request

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -10,7 +10,7 @@ export async function getImagesByQuery(query) {
             q: query,
             image_type: 'photo',
             orientation: 'horizontal',
-            satisfies: 'true',
+            safesearch : 'true',
         },
     });
 
